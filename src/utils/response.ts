f29c563edb2dddfa7933a16202cc27e9501dfc9e
omotega <tomoyibo@gmail.com>
2023-01-22 01:53:40 +0100

    feat:created different application responses

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,23 +1,30 @@
 import { Request, Response } from 'express';
-function errorResponse (res:Response,error: string,statusCode:number) {
-    const resobj = {statusCode,error};
-    return res.status(statusCode).send(resobj);
+function errorResponse(res: Response, statusCode: number, error: string) {
+  const resobj = { statusCode, error };
+  return res.status(statusCode).send(resobj);
 }
 
-function successResponse(res:Response,statusCode:number,message:string,data:any = []) {
-    const resobj = {statusCode,message,data};
-    return res.status(statusCode).send(resobj);
+function successResponse(
+  res: Response,
+  statusCode: number,
+  message: string,
+  data: any = []
+) {
+  const resobj = { statusCode, message, data };
+  return res.status(statusCode).send(resobj);
 }
 
-function handleError(req:Request,err:any) {
-    console.log(`
-        Errormessage: ${JSON.stringify(err.message)},caught at: ${JSON.stringify(req.path)}
-    `)
+function validationErrors(res: Response, statusCode: number, error: any) {
+  const resobj = { statusCode, error };
+  return res.status(statusCode).send(resobj);
 }
 
-export {
-    errorResponse,
-    successResponse,
-    handleError,
+function handleError(req: Request, err: any) {
+  console.log(`
+        Errormessage: ${JSON.stringify(
+          err.message
+        )},caught at: ${JSON.stringify(req.path)}
+    `);
 }
 
+export { errorResponse, successResponse, handleError,validationErrors };
