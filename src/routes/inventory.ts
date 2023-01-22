9bc3bb4c21a50fc0789fe7b64fbcc708a177769f
omotega <tomoyibo@gmail.com>
2023-01-22 09:23:17 +0100

    feat:added edit inventory route

--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -2,8 +2,9 @@ import { Router } from 'express'
 
 const inventoryRouter = Router();
 
-import { createInventory } from '../controllers/inventory'
+import { createInventory,editInventory } from '../controllers/inventory'
 
+inventoryRouter.route('/edit/:id').put(editInventory);
 inventoryRouter.route('/create').post(createInventory);
 
 
