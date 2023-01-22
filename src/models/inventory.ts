af813f6f96ea305fe0485630294364ae10defed7
omotega <tomoyibo@gmail.com>
2023-01-22 04:21:30 +0100

    feat:created inventory model

--- src/models/inventory.ts
+++ src/models/inventory.ts
@@ -2,12 +2,15 @@ import mongoose from 'mongoose';
 import { Inventory } from '../utils/interface';
 
 const inventorySchema = new mongoose.Schema({
-  stock: {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    size: { type: String, required: true },
-    stockNumber: { type: Number, required: true },
-  },
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  size: { type: String, required: true },
+  stockNumber: { type: Number, required: true },
+  stockPrice: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
+  comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
+},{
+  timestamps:true,
 });
 
 export default mongoose.model<Inventory>('Inventory', inventorySchema);
