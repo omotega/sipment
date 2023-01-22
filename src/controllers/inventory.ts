31fd522a469c3e7ab1bab878bd09593ff76fde0c
omotega <tomoyibo@gmail.com>
2023-01-22 09:22:27 +0100

    feat:created editinventory

--- src/controllers/inventory.ts
+++ src/controllers/inventory.ts
@@ -15,17 +15,46 @@ export async function createInventory(req: Request, res: Response) {
       stockPrice,
       totalPrice,
     });
-    return successResponse(res, 201, 'inventory created successfully',inventory);
+    return successResponse(
+      res,
+      201,
+      'inventory created successfully',
+      inventory
+    );
   } catch (error) {
     handleError(req, error);
     return errorResponse(res, 500, 'Something Happened');
   }
 }
 
-export async function editInventory(req:Request,res:Response) {
-    try {
-        
-    } catch (error) {
-        
+export async function editInventory(req: Request, res: Response) {
+  try {
+    let { name, description, size, stockNumber, stockPrice } = req.body;
+    const { id } = req.params;
+    const inventory = await model.Inventory.findById(id);
+    let totalPrice;
+    if (!inventory) return errorResponse(res, 400, 'inventory not found');
+    if (stockNumber) {
+      totalPrice = await Helper.totalPrice(stockNumber, inventory.stockPrice);
+    } else if (stockPrice) {
+      totalPrice = await Helper.totalPrice(stockPrice, inventory.stockNumber);
+    } else {
+      totalPrice = await Helper.totalPrice(stockNumber, stockPrice);
     }
+
+    await model.Inventory.findByIdAndUpdate(
+      id,
+      { name, description, size, stockNumber, stockPrice, totalPrice },
+      { new: true }
+    );
+    return successResponse(
+      res,
+      200,
+      'Inventory updated successfully',
+      inventory
+    );
+  } catch (error) {
+    handleError(req, error);
+    return errorResponse(res, 500, 'Something Happened');
+  }
 }
