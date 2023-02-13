44a34fcc64d69d4dadf8cb38b0c96e4462cb77f0
omotega <tomoyibo@gmail.com>
2023-02-13 14:35:47 +0100

    added warehouse router to the route index file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import userRouter from './user';
 import inventoryRouter from './inventory';
 import commentRouter from './comment';
+import warehouseRouter from './warehouse';
 
-const router = { userRouter,inventoryRouter,commentRouter };
+const router = { userRouter,inventoryRouter,commentRouter,warehouseRouter };
 
 export default router;
