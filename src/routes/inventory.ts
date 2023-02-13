a91293a869eab750207d61cf8e8d8ece007a5dac
omotega <tomoyibo@gmail.com>
2023-02-13 14:37:40 +0100

    feat: created getinventorybyid route

--- src/routes/inventory.ts
+++ src/routes/inventory.ts
@@ -2,14 +2,14 @@ import { Router } from 'express'
 
 const inventoryRouter = Router();
 
-import { createInventory,editInventory,deleteInventory,allInventory } from '../controllers/inventory'
+import { createInventory,editInventory,deleteInventory,allInventory,getInventoryById } from '../controllers/inventory'
 import { authGuard } from '../middleware/auth';
 
 
-inventoryRouter.route('/edit/:inventoryId').put(authGuard,editInventory);
 inventoryRouter.route('/').get(allInventory);
 inventoryRouter.route('/create').post(authGuard,createInventory);
-inventoryRouter.route('/inventoryId').delete(authGuard,deleteInventory);
+inventoryRouter.route('/edit/:inventoryId').put(authGuard,editInventory);
+inventoryRouter.route('/:inventoryId').get(getInventoryById).delete(authGuard,deleteInventory);
 
 
 export default inventoryRouter;
