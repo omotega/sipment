46316a959c77efb4c9bcd6015fa232ac3ae3e38e
omotega <tomoyibo@gmail.com>
2023-02-13 14:30:38 +0100

    feat: added createwarehouse function

--- src/controllers/warehouse.ts
+++ src/controllers/warehouse.ts
@@ -6,16 +6,11 @@ export async function createWarehouse(req: Request, res: Response) {
   try {
     const { location } = req.body;
     const warehouse = await model.Warehouse.create({ location });
-    return successResponse(res, 201, 'warehouse created successfully');
+    return successResponse(res, 201, 'warehouse created successfully',warehouse);
   } catch (error) {
     handleError(req, error);
     return errorResponse(res, 500, 'Something Happened');
   }
 }
 
-// export async function assignInventory(req: Request, res: Response) {
-//   try {
-//     const { InventoryId } = req.body
-    
-//   } catch (error) {}
-// }
+
