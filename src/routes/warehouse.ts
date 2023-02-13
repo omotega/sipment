69e7744e1bd5b42e22d5fe7df9fb23b0fb94dcc5
omotega <tomoyibo@gmail.com>
2023-02-13 14:34:43 +0100

    feat:added assigninventory route

--- src/routes/warehouse.ts
+++ src/routes/warehouse.ts
@@ -2,10 +2,10 @@ import { Router } from 'express'
 
 const warehouseRouter = Router();
 
-import { createWarehouse } from '../controllers/warehouse'
+import { createWarehouse,assignInventory } from '../controllers/warehouse'
 import { authGuard } from '../middleware/auth'
 
 warehouseRouter.route('/create').post(authGuard,createWarehouse);
-
+warehouseRouter.route('/assign').post(authGuard,assignInventory);
 
 export default warehouseRouter
