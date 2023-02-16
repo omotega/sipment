60f21d58b676a711d1b8c48da69994752b3e819a
omotega <tomoyibo@gmail.com>
2023-02-16 11:13:43 +0100

    chore:added inventory data

--- src/__tests__/fixtures/inventory_test_data.ts
+++ src/__tests__/fixtures/inventory_test_data.ts
@@ -11,10 +11,54 @@ function totalPrice(stockNumber:number,stockPrice:number) {
 
 const stockNumber = 400;
 const stockPrice = 100;
-export const inventoryPayload = {
+export const inventoryPayload1 = {
   name: 'inventory1',
   description: 'this is the first inventory',
   size: 'l',
+  stockNumber: stockNumber,
+  stockPrice: stockPrice,
+  totalPrice: totalPrice(stockNumber, stockPrice),
+  user_id: userId,
+  comment: [commentId],
+};
+
+export const inventoryPayload2 = {
+  name: 'inventory2',
+  description: 'this is the secondary inventory',
+  size: 'medium',
+  stockNumber: stockNumber,
+  stockPrice: stockPrice,
+  totalPrice: totalPrice(stockNumber, stockPrice),
+  user_id: userId,
+  comment: [commentId],
+};
+
+export const inventoryPayload3 = {
+  name: 'inventory3',
+  description: 'this is the third inventory',
+  size: 'small',
+  stockNumber: stockNumber,
+  stockPrice: stockPrice,
+  totalPrice: totalPrice(stockNumber, stockPrice),
+  user_id: userId,
+  comment: [commentId],
+};
+
+export const inventoryPayload4 = {
+  name: 'inventory4',
+  description: 'this is the fourth inventory',
+  size: 'larger',
+  stockNumber: stockNumber,
+  stockPrice: stockPrice,
+  totalPrice: totalPrice(stockNumber, stockPrice),
+  user_id: userId,
+  comment: [commentId],
+};
+
+export const inventoryPayload5 = {
+  name: 'inventory4',
+  description: 'this is the fifth inventory',
+  size: 'largeest',
   stockNumber: stockNumber,
   stockPrice: stockPrice,
   totalPrice: totalPrice(stockNumber, stockPrice),
