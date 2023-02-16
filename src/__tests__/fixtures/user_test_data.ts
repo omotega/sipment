0fb4a974ff5fdfb836ef66932e7b0ba8a60219d0
omotega <tomoyibo@gmail.com>
2023-02-16 11:14:29 +0100

    chore:added user data

---
+++ src/__tests__/fixtures/user_test_data.ts
@@ -0,0 +1,23 @@
+export const user1 = {
+  username: 'omoyibotega22',
+  email: 'tomoyi@gmail.com',
+  password: '123456789',
+};
+
+export const user2 = {
+  username: 'omoyibotega23',
+  email: 'tomoyis@gmail.com',
+  password: '1234567890',
+};
+
+export const user3 = {
+  username: 'omoyibotega24',
+  email: 'tomoyiss@gmail.com',
+  password: '12345678913',
+};
+
+export const user4 = {
+  username: 'omoyibotega256',
+  email: 'tomoyismg@gmail.com',
+  password: '123456789125',
+};
