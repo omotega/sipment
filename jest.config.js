bb8fb41f45a0df884d2d07c930bb62b2d7dbad66
omotega <tomoyibo@gmail.com>
2023-02-16 11:20:46 +0100

    configure jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 /**@types {import ('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
     preset:"ts-jest",
-    testEnviroment:"node",
+    testEnvironment:"node",
     testMatch:['**/**/*.test.ts'],
     verbose:true,
     forceExit:true,
